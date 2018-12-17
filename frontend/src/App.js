c6b78015963c73c6cf6ec3a8f489354a9246ecff
Progga1 <andreasfender@cs.au.dk>
2018-12-17 15:12:27 +0100

    Now the todo component can be shown moved and increased or decreased in width

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,55 @@
 import React, { Component } from 'react';
 import Websocket from 'react-websocket';
 import WhatIf from './Components/WhatIf';
+import TodoProgress from './Components/TodoProgress';
+import { Progress, Button } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
     constructor(props){
         super(props);
 
+        this.container = React.createRef();
+
         this.nodeOne = React.createRef();
         this.nodeTwo = React.createRef();
+        this.nodeThree = React.createRef();
 
         this.dynamicWhatIfNode = React.createRef();
         this.staticWhatIfNode = React.createRef();
+        this.progressBar = React.createRef();
     }
 
-    handleMessages(message){
+    /*handleMessages(message){
         this.dynamicWhatIfNode.current.handleMessages(message);
         this.staticWhatIfNode.current.handleMessages(message);
+    }*/
+
+    handleMessages(message){
+        var jsonData = JSON.parse(message);
+        if (jsonData.type == "Creation") {
+            //TODO: Make it so that new TodoProgress elements are created in here
+            var transformedWidth = this.container.current.offsetWidth * jsonData.width;
+            this.progressBar.current.initialize(transformedWidth, jsonData.position);
+        }
     }
 
     render() {
         return (
-            <div>
-                {/* NB! These props create an implicit dependency between the view and the interaction tracker and should
+            <div ref={this.container}>
+                {/* NB! These props ceate an implicit dependency between the view and the interaction tracker and should
               therefore be abolished as quickly as possible! */}
-                <div style={{position:"absolute", width:"150px", height:"300px", top:170, left:350 }} ref={this.nodeOne}>
+                {/* <div style={{position:"absolute", width:"150px", height:"300px", top:170, left:350 }} ref={this.nodeOne}>
                     <WhatIf identity={1} yLabel="Number of TODOs" domain={[0, 5]} initialValues={[[1, 1], [2, 1]]} yAxisTickCount={5}
                         parentRef={this.nodeOne} ref={this.dynamicWhatIfNode}></WhatIf>
                 </div>
                 <div style={{position:"absolute", width:"150px", height:"300px", top:70, left:250 }} ref={this.nodeTwo}>
                     <WhatIf identity={2} yLabel="Time in minutes" domain={[0, 120]} initialValues={[[1, 20], [2, 20]]} yAxisTickCount={6}
                         parentRef={this.nodeTwo} ref={this.staticWhatIfNode}></WhatIf>
+                </div> */}
+
+                <div style={{position:"absolute", width:"150px", height:"300px", top:70, left:250 }} ref={this.nodeThree}>
+                    <TodoProgress identity={3} parentRef={this.nodeThree} ref={this.progressBar}></TodoProgress>
                 </div>
                 <Websocket url="ws://localhost:1336" onMessage={this.handleMessages.bind(this)} />
             </div>
